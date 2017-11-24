a62aec99367a203748d20fdccf22d7eb1170e418
Mark Reid <contact@markreid.com.au>
2017-11-25 10:38:29 +1100

    Force constraints in the migration to deal with sequelize bug

--- migrations/20171115234757-nullable-images.js
+++ migrations/20171115234757-nullable-images.js
@@ -9,6 +9,23 @@ module.exports = {
       queryInterface.changeColumn('Reports', 'postImages', {
         type: Sequelize.DataTypes.STRING,
         allowNull: true,
+      }))
+    // sqlite3 migrations drop constraints, see:
+    // https://github.com/sequelize/sequelize/issues/5007
+    .then(() =>
+      queryInterface.changeColumn('Reports', 'id', {
+        type: Sequelize.DataTypes.INTEGER,
+        unique: true,
+        primaryKey: true,
+        autoIncrement: true,
+      }))
+    .then(() =>
+      queryInterface.addConstraint('Reports', ['postGuid'], {
+        type: 'unique',
+      }))
+    .then(() =>
+      queryInterface.addConstraint('Reports', ['id'], {
+        type: 'unique',
       })),
   down: (queryInterface, Sequelize) => Promise.resolve()
     .then(() =>
@@ -20,5 +37,22 @@ module.exports = {
       queryInterface.changeColumn('Reports', 'postImages', {
         type: Sequelize.DataTypes.STRING,
         allowNull: false,
+      }))
+    // sqlite3 migrations drop constraints, see:
+    // https://github.com/sequelize/sequelize/issues/5007
+    .then(() =>
+      queryInterface.changeColumn('Reports', 'id', {
+        type: Sequelize.DataTypes.INTEGER,
+        unique: true,
+        primaryKey: true,
+        autoIncrement: true,
+      }))
+    .then(() =>
+      queryInterface.addConstraint('Reports', ['postGuid'], {
+        type: 'unique',
+      }))
+    .then(() =>
+      queryInterface.addConstraint('Reports', ['id'], {
+        type: 'unique',
       })),
 };
